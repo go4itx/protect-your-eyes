45b02c588d5b81fa19493467abcfaeead2c9d752
go4it <1109324207@qq.com>
2021-06-30 18:01:00 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# protect-your-eyes
+## protect-your-eyes
+>>>
+>1. 安装 npm install
+>2. 运行 npm start
+>3. 打包 npm run make
+>>>
 electron开发的桌面应用，窗口置顶透明，每隔45分提示“休息一下”
+#
+当然自己可以继续开发，比如：桌面歌词，电脑全屏弹幕......
+### 效果图
+![效果图](./result.gif) 
+result.gif
---
+++ src/index.js
@@ -0,0 +1,63 @@
+const {app, screen, BrowserWindow} = require('electron');
+const path = require('path');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+    app.quit();
+}
+
+const createWindow = () => {
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        title: "保护你的眼睛",
+        resizable: true,
+        frame: false,
+        transparent: true, // 是否透明
+        width: screen.getPrimaryDisplay().workAreaSize.width-5,
+        height: screen.getPrimaryDisplay().workAreaSize.height-5,
+        autoHideMenuBar: true,
+        alwaysOnTop: true,  // 窗口置顶
+        webPreferences: {
+            webviewTag: true,
+            nodeIntegration: true,
+            //contextIsolation: false,
+            //enableRemoteModule: true,
+            //devTools:false,
+        }
+    });
+
+    // and load the index.html of the app.
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+    // 事件穿透
+    mainWindow.setIgnoreMouseEvents(true);
+    // Open the DevTools.
+    //mainWindow.webContents.openDevTools();
+    //mainWindow.webContents.closeDevTools();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
+
